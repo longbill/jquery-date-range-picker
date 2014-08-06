9468d7ddac799140c0f88ccdadb418b341970bd2
longbill.cn@gmail.com <liuchun643210>
2014-08-07 00:03:36 +0800

    merge pull request for added de translation

--- jquery.daterangepicker.js
+++ jquery.daterangepicker.js
@@ -108,6 +108,39 @@
 			'default-less' : 'Seleziona un intervallo minore di %d giorni',
 			'default-range' : 'Seleziona un intervallo compreso tra i %d e i %d giorni',
 			'default-default': 'Seleziona un intervallo di date'
+		},
+		'de':
+		{
+			'selected': 'Auswahl:',
+			'day':'Tag',
+			'days': 'Tage',
+			'apply': 'Schließen',
+			'week-1' : 'MO',
+			'week-2' : 'DI',
+			'week-3' : 'MI',
+			'week-4' : 'DO',
+			'week-5' : 'FR',
+			'week-6' : 'SA',
+			'week-7' : 'SO',
+			'month-name': ['JANUAR','FEBRUAR','MÄRZ','APRIL','MAI','JUNI','JULI','AUGUST','SEPTEMBER','OKTOBER','NOVEMBER','DEZEMBER'],
+			'shortcuts' : 'Schnellwahl',
+			'past': 'Vorherige',
+			'following':'Folgende',
+			'previous' : 'Vorherige',
+			'prev-week' : 'Woche',
+			'prev-month' : 'Monat',
+			'prev-year' : 'Jahr',
+			'next':'Nächste',
+			'next-week':'Woche',
+			'next-month':'Monat',
+			'next-year':'Jahr',
+			'less-than' : 'Datumsbereich darf nicht größer sein als %d Tage',
+			'more-than' : 'Datumsbereich darf nicht kleiner sein als %d Tage',
+			'default-more' : 'Bitte mindestens %d Tage auswählen',
+			'default-single' : 'Bitte ein Datum auswählen',
+			'default-less' : 'Bitte weniger als %d Tage auswählen',
+			'default-range' : 'Bitte einen Datumsbereich zwischen %d und %d Tagen auswählen',
+			'default-default': 'Bitte ein Start- und Enddatum auswählen'
 		}
 	};
 
