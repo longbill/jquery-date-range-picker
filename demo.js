f89945f0a6e540d7648b9480bed9563658b0b070
Chunlong <longbill.cn@gmail.com>
2015-06-26 01:06:11 +0800

    fix issue #68

--- demo.js
+++ demo.js
@@ -1,6 +1,12 @@
 $(function()
 {
-	
+
+	if (!window['console'])
+	{
+		window.console = {};
+		window.console.log = function(){};
+	}
+		
 	/*
 	define a new language named "custom"
 	*/
@@ -38,7 +44,53 @@ $(function()
 	
 	$('#date-range0').dateRangePicker(
 	{
+	}).bind('datepicker-first-date-selected', function(event, obj)
+	{
+		/* This event will be triggered when first date is selected */
+		console.log('first-date-selected',obj);
+		// obj will be something like this:
+		// {
+		// 		date1: (Date object of the earlier date)
+		// }
+	})
+	.bind('datepicker-change',function(event,obj)
+	{
+		/* This event will be triggered when second date is selected */
+		console.log('change',obj);
+		// obj will be something like this:
+		// {
+		// 		date1: (Date object of the earlier date),
+		// 		date2: (Date object of the later date),
+		//	 	value: "2013-06-05 to 2013-06-07"
+		// }
+	})
+	.bind('datepicker-apply',function(event,obj)
+	{
+		/* This event will be triggered when user clicks on the apply button */
+		console.log('apply',obj);
+	})
+	.bind('datepicker-close',function()
+	{
+		/* This event will be triggered before date range picker close animation */
+		console.log('before close');
+	})
+	.bind('datepicker-closed',function()
+	{
+		/* This event will be triggered after date range picker close animation */
+		console.log('after close');
+	})
+	.bind('datepicker-open',function()
+	{
+		/* This event will be triggered before date range picker open animation */
+		console.log('before open');
+	})
+	.bind('datepicker-opened',function()
+	{
+		/* This event will be triggered after date range picker open animation */
+		console.log('after open');
 	});
+
+
 
 	$('#date-range1').dateRangePicker(
 	{
