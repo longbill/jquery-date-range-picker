ea450c7126c095ab49942b1063c0f82a6ce3d984
Igor Peshkov <igor.peshkov@gmail.com>
2016-07-18 13:05:35 +0300

    Fix startDate and endDate intial options to using moment locale.

--- src/jquery.daterangepicker.js
+++ src/jquery.daterangepicker.js
@@ -706,8 +706,8 @@
 
 		if (!opt.showTopbar) opt.autoClose = true;
 
-		if (opt.startDate && typeof opt.startDate == 'string') opt.startDate = moment(opt.startDate,opt.format).toDate();
-		if (opt.endDate && typeof opt.endDate == 'string') opt.endDate = moment(opt.endDate,opt.format).toDate();
+		if (opt.startDate && typeof opt.startDate == 'string') opt.startDate = moment(opt.startDate,opt.format,moment.locale(opt.language)).toDate();
+		if (opt.endDate && typeof opt.endDate == 'string') opt.endDate = moment(opt.endDate,opt.format,moment.locale(opt.language)).toDate();
 
 		var langs = getLanguages();
 		var box;
