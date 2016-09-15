7034adeb9913629e9581487956ee09d525991b98
Hemanth S R <hemanthfeb25@gmail.com>
2016-09-15 14:41:17 +0530

    Added Japanese translation

--- src/jquery.daterangepicker.js
+++ src/jquery.daterangepicker.js
@@ -620,7 +620,43 @@
 			'time': '日期',
 			'hour': '小時',
 			'minute': '分鐘'
-		}
+		},
+		'ja':
+        	{
+            		'selected': '選択しました:',
+	            	'day':'日',
+	            	'days': '日々',
+	            	'apply': '閉じる',
+	            	'week-1' : '日',
+	            	'week-2' : '月',
+	            	'week-3' : '火',
+	            	'week-4' : '水',
+	            	'week-5' : '木',
+	            	'week-6' : '金',
+	            	'week-7' : '土',
+	            	'month-name': ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+	            	'shortcuts' : 'クイック選択',
+	            	'past': '過去',
+	            	'following':'将来',
+	            	'previous' : '&nbsp;&nbsp;&nbsp;',
+	            	'prev-week' : '先週、',
+	            	'prev-month' : '先月',
+	            	'prev-year' : '昨年',
+	            	'next': '&nbsp;&nbsp;&nbsp;',
+	            	'next-week':'来週',
+	            	'next-month':'来月',
+	            	'next-year':'来年',
+	            	'less-than' : '日付の範囲は ％d 日以上にすべきではありません',
+	            	'more-than' : '日付の範囲は ％d 日を下回ってはいけません',
+	            	'default-more' : '％d 日よりも長い期間を選択してください',
+	            	'default-less' : '％d 日未満の期間を選択してください',
+	            	'default-range' : '％d と％ d日の間の日付範囲を選択してください',
+	            	'default-single':'日付を選択してください',
+	            	'default-default': '日付範囲を選択してください',
+	            	'time': '時間',
+	            	'hour': '時間',
+	            	'minute': '分'
+	        }
 	};
 
 	$.fn.dateRangePicker = function(opt)
