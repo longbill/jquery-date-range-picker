f7d2367b45dc68641232031190c54541402bb64f
Irfan Maulana <mazipanneh@gmail.com>
2017-07-03 14:45:32 +0700

    Gulp setup sass and autoprefixer (#358)
    - Install using yarn
    - #355 Using SASS instead of CSS for development
    - #357 Setup autoprefixer in Gulpfile
    - Change gulp-minify-css to gulp-clean-css because deprecated
    - Add browsersync for hot reload development
    - Change link css and js in demo for hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,12 @@
         del = require('del'),
         beautify = require("gulp-jsbeautifier"),
         uglify = require("gulp-uglify"),
-        minifyCSS = require("gulp-minify-css"),
+        cleanCSS = require('gulp-clean-css'),
         rename = require("gulp-rename"),
-        header = require('gulp-header');
+        header = require('gulp-header'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        browserSync = require('browser-sync').create();
 
     var pkg = require('./package.json');
     var banner = ['/**',
@@ -27,11 +30,20 @@
 
     gulp.task('dist:clean', function () {
         del.sync('./dist', {force: true});
+        del.sync('./src/*.css', {force: true});
     });
 
-    gulp.task('dist:styles', ['dist:clean'], function () {
-        return gulp.src('./src/*.css')
-            .pipe(minifyCSS())
+    gulp.task('dist:sass', ['dist:clean'], function() {
+        return gulp
+            .src(['./src/*.scss'])
+            .pipe(sass({outputStyle: 'expanded'}))
+            .pipe(autoprefixer({browsers: ['last 2 versions', 'ie 8']}))
+            .pipe(gulp.dest('./dist'));
+    });
+
+    gulp.task('dist:styles', ['dist:clean', 'dist:sass'], function () {
+        return gulp.src('./dist/*.css')
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
             .pipe(rename('daterangepicker.min.css'))
             .pipe(gulp.dest('./dist'))
             .on('error', gutil.log)
@@ -46,9 +58,23 @@
             .on('error', gutil.log)
     });
 
+    gulp.task('dev:serve', ['dist:clean', 'dist:sass', 'dist:styles'], function() {
+        browserSync.init({
+            port: 3000,
+            server: "./"
+        });
+        gulp.watch('./src/*.scss', ['simple-sass']);
+        gulp.watch('./*.html').on('change', browserSync.reload);
+    });
 
-    gulp.task('default', ['dist:clean', 'dist:styles', 'dist:script'], function (cb) {
-        gutil.log('Info :', gutil.colors.green('Distribution files are ready!'));
+    gulp.task('default', ['dist:clean', 'dist:sass', 'dist:styles', 'dist:script'], function (cb) {
+        gutil.log('Info :', gutil.colors.green('Distribution files v.' + pkg.version + ' are ready!'));
+        cb(null)
+    });
+
+    gulp.task('dev', ['dist:clean', 'dist:sass', 'dist:styles', 'dist:script', 'dev:serve'], function (cb) {
+        gutil.log('Info :', gutil.colors.green('Build complete!'));
+        gutil.log('Info :', gutil.colors.green('Opening browser on http://localhost:3000/'));
         cb(null)
     });
 
